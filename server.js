67467d67b2b980329508dc8ce99d8bce775ffd1c
Jelena <jg55932@fer.hr>
2025-11-14 16:22:04 +0100

    Popravka CORS za session support

--- server.js
+++ server.js
@@ -30,9 +30,14 @@ app.use(express.static('public'));
 
 // CORS middleware - omogućava pristup s bilo koje domene
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  const origin = req.headers.origin;
+  res.header('Access-Control-Allow-Origin', origin || '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
